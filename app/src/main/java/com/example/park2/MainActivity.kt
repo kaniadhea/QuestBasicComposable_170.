fb13ef915f33a1a03fa9efad9abc2795a5210480
kaniadhea <kaniadhea7@gmail.com>
2024-10-18 18:40:34 +0700

    Menampilkan teks "LOGIN" dengan ukuran font yang sangat besar (75 sp)

--- app/src/main/java/com/example/park2/MainActivity.kt
+++ app/src/main/java/com/example/park2/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.park2.ui.theme.Park2Theme
 
 class MainActivity : ComponentActivity() {
@@ -27,9 +39,40 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
 fun BasicColumn(modifier: Modifier = Modifier){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 50.dp)
+    )
+    {
+        Text("LOGIN",
+        fontSize = 75.sp,
+            fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.padding(10.dp))
+
+        Text("Ini adalah halaman login",
+            fontSize = 30.sp,
+            fontStyle = FontStyle.Italic
+        )
+
+        Spacer(modifier = Modifier.padding(20.dp))
+
+
+        Image(painter = painterResource(id = R.drawable.umy),
+            contentDescription = " ",
+            modifier = Modifier.clip(CircleShape)
+        )
+
+
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
